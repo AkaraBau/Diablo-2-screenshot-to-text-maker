c04e7d8a016ef60534a30a25e6926f4b41f2a260
AkaraBau <fiffeandersson@gmail.com>
2024-07-03 18:44:51 +0200

    Creation of project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AkarasDegenStuff
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            ScreenShotToText.DoIt();
+        }
+    }
+}
---
+++ ScreenShotToText.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AkarasDegenStuff
+{
+    public class ScreenShotToText
+    {
+        public static void DoIt()
+        {
+        Console.ReadKey(); 
+        }
+    }
+}
