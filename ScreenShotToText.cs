7a2dd875bd405b602f4ba133fc0477087308ef2c
AkaraBau <fiffeandersson@gmail.com>
2024-07-07 12:41:04 +0200

    Adding of tessaract + ocr

--- ScreenShotToText.cs
+++ ScreenShotToText.cs
@@ -1,4 +1,8 @@
 using System;
+using System.IO;
+using NLog.LayoutRenderers;
+using TesseractSharp;
+using TesseractSharp.Hocr;
 
 namespace AkarasDegenStuff
 {
@@ -6,7 +10,17 @@ namespace AkarasDegenStuff
     {
         public static void DoIt()
         {
-        Console.ReadKey(); 
+            var input = @"C:\Users\fide_\Desktop\d2 items/6sl1.jpg";
+
+            
+            using (var stream = Tesseract.ImageToTxt(input, languages: new[] { Language.English, Language.French }))
+            {
+                Console.Write(stream.ToString()); 
+            }
+
+            
+            Console.ReadLine();
         }
+
     }
 }
