27feeee7b8aff1eceea3d9963c00b7400e083216
AkaraBau <fiffeandersson@gmail.com>
2024-07-14 19:39:52 +0200

    dasdasd

---
+++ Utils.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq; //accessing case sensitive check
+using System.Runtime.InteropServices;
+using NLog.LayoutRenderers;
+using TesseractSharp;
+using TesseractSharp.Core;
+using TesseractSharp.Hocr;
+
+namespace AkarasDegenStuff
+{
+    public class Utils
+    {
+        public static void PrintList(){
+            
+        }
+       
+    }
+}
