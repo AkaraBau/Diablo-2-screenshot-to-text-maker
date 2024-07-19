8583c25d266d43a998cebe7dcaac35890ac16882
AkaraBau <fiffeandersson@gmail.com>
2024-07-19 13:31:10 +0200

    preparing for method single add

--- ScreenShotToText.cs
+++ ScreenShotToText.cs
@@ -18,21 +18,25 @@ namespace AkarasDegenStuff
             
             
             var input = @"C:\Users\fide_\Desktop\Programmering\Tessaract\";
+            
             string item = null;
+            string[]data = new string[14];
             using (var stream = Tesseract.ImageToTxt(input, languages: new[] { Language.English, Language.French }))
             {
                 StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8); //making stream -> string
 
                 item = reader.ReadToEnd(); //making stream -> string 
+                item = Utils.RemoveAllWhiteSpace(item);
                 item = Utils.ChangeLetters(item); 
                 item = Utils.ShortenString(item);
+                data = item.Split(new[] { '\n' }, StringSplitOptions.None); //split string into array of strings 
+                data = data.Where(x => !String.IsNullOrWhiteSpace(x)).ToArray(); // removing whitespace
+                Belt belt = new Belt(data); // creation of a Belt
             }
 
-            string[] data = item.Split(new[] { '\n' }, StringSplitOptions.None); //split string into array of strings   
+            
 
-            data = data.Where(x => !String.IsNullOrWhiteSpace(x)).ToArray(); // removing whitespace
-
-            Belt belt = new Belt(data); // creation of a Belt
+            
 
 
             Console.ReadLine();
