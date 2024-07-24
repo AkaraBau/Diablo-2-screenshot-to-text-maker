2f163a279193820d51b1402e2a93550663c7d22e
AkaraBau <fiffeandersson@gmail.com>
2024-07-24 12:21:59 +0200

    Adding of all items stat2>

--- Belt.cs
+++ Belt.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Formats.Asn1;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
 using NLog.LayoutRenderers;
@@ -17,17 +18,17 @@ namespace AkarasDegenStuff
         public string req1 { get; set; }
         public int level { get; set; }
         public string stat1 { get; set; }
-        public int amount1 { get; set; }
+        public int? amount1 { get; set; }
         public string stat2 { get; set; }
-        public int amount2 { get; set; }
+        public int? amount2 { get; set; }
         public string stat3 { get; set; }
-        public int amount3 { get; set; }
+        public int? amount3 { get; set; }
         public string stat4 { get; set; }
-        public int amount4 { get; set; }
+        public int? amount4 { get; set; }
         public string stat5 { get; set; }
-        public int amount5 { get; set; }
+        public int? amount5 { get; set; }
         public string stat6 { get; set; }
-        public int amount6 { get; set; }
+        public int? amount6 { get; set; }
         public string stat7 { get; set; }
         public int? amount7 { get; set; }
 
@@ -46,48 +47,111 @@ namespace AkarasDegenStuff
 
             amount2 = Utils.ExtractIntFromString(data[8]);
             stat2 = Utils.RemoveNumbers(data[8]);
-            
+
             amount3 = Utils.ExtractIntFromString(data[9]);
             stat3 = Utils.RemoveNumbers(data[9]);
 
             amount4 = Utils.ExtractIntFromString(data[10]);
             stat4 = Utils.RemoveNumbers(data[10]);
-            
+
             amount5 = Utils.ExtractIntFromString(data[11]);
             stat5 = Utils.RemoveNumbers(data[11]);
-            
+
             amount6 = Utils.ExtractIntFromString(data[12]);
             stat6 = Utils.RemoveNumbers(data[12]);
 
-            string newName = $"{amount1}{stat1}{stat2}{stat3}{stat4}{stat5}{stat6}"; 
-            if (stat2 == "FCR" || stat2 == "OFCR" ){
-                newName = $"{amount2}{stat2}{stat3}{stat4}{stat5}{stat6}";
-            }
-            else if (stat2 == "CDMG")
+            if (data.Length == 9)
             {
-                newName =  $"{amount1}{stat1}{stat3}{stat4}{stat5}{stat6}"; 
+                amount3 = null;
+                stat3 = "";
+                amount4 = null;
+                stat4 = "";
+                amount5 = null;
+                stat5 = "";
+                amount6 = null;
+                stat6 = "";
+                amount7 = null;
+                stat7 = "";
             }
-            name = newName;
-
-            if (data.Length == 13)
+            else if (data.Length == 10)
+            {
+                amount4 = null;
+                stat4 = "";
+                amount5 = null;
+                stat5 = "";
+                amount6 = null;
+                stat6 = "";
+                amount7 = null;
+                stat7 = "";
+            }
+            else if (data.Length == 11)
+            {
+                amount5 = null;
+                stat5 = "";
+                amount6 = null;
+                stat6 = "";
+                amount7 = null;
+                stat7 = "";
+            }
+            else if (data.Length == 12)
+            {
+                amount6 = null;
+                stat6 = "";
+                amount7 = null;
+                stat7 = "";
+            }
+            else if (data.Length == 13)
             {
                 amount7 = null;
-                stat7 = " ";
+                stat7 = "";
             }
             else if (data.Length == 14)
             {
                 amount7 = Utils.ExtractIntFromString(data[13]);
                 stat7 = Utils.RemoveNumbers(data[13]);
             }
-            else if (data.Length < 13 || data.Length > 14)
+            else if (data.Length < 8 || data.Length > 14)
             {
                 throw new ArgumentException("The data array does not contain enough elements.");
             }
+            ////////////////////////////////////////////////////////////////////////////////////////
+            ////////////////////////////////////////////////////////////////////////////////////////
+            string newName = $"{amount1}{stat1}{stat2}{stat3}{stat4}{stat5}{stat6}";
+            if (stat2 == "FCR" || stat2 == "OFCR" || data.Length >= 13)
+            {
+                newName = $"{amount2}{stat2}{stat3}{stat4}{stat5}{stat6}";
+            }
+            else if (stat2 == "CDMG")
+            {
+                newName = $"{amount1}{stat1}{stat3}{stat4}{stat5}{stat6}";
+            }
+
+            if (data.Length < 13)
+            {
+                newName = $"{stat1}{stat2}";
+            }
+            name = newName;
         }
         public override string ToString()
         {
-            string result = $"{name}/{type}/{defenseAmount}{defense}/{level}{req1}\t{amount1}{stat1}/{amount2}{stat2}/{amount3}{stat3}/{amount4}{stat4}/{amount5}{stat5}/{amount6}{stat6}";
-            if (stat7 != " ")
+            string result = $"{name}/{type}/{defenseAmount}{defense}/{level}{req1}\t{amount1}{stat1}/{amount2}{stat2}";
+            if (stat3 != "")
+            {
+                result += $"/{amount3}{stat3}";
+            }
+            else if (stat4 != "")
+            {
+                result += $"/{amount4}{stat4}";
+            }
+            else if (stat5 != "")
+            {
+                result += $"{amount5}{stat5}";
+            }
+            else if (stat6 != "")
+            {
+                result += $"/{amount6}{stat6}";
+            }
+            else if (stat7 != "")
             {
                 result += $"/{amount7}{stat7}";
             }
